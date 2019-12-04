e081a2c1c22c05700ac335a06290c2a11f5a0ae6
kyungmin-Lee-knu <58453290+kyungmin-Lee-knu@users.noreply.github.com>
2019-12-04 14:30:07 +0900

    Add files via upload

---
+++ homework2_12_8.m
@@ -0,0 +1,10 @@
+x=[1.2;1.2]; %x,y°ª
+j=[-2*x(1)+1 -1;2*x(1)-5*x(2) -5*x(1)+1] %Jocobian Matrix
+f=[-x(1)^2+x(1)-x(2)+0.5;x(1)^2+(-5)*x(1)*x(2)+x(2)]%f1(x,y);f2(x,y)
+inv(j)  %[j]^1
+%Xnew = Xold-j\f
+x=x-j\f %first order
+x=x-j\f %second order
+x=x-j\f %third order
+x=x-j\f %fourth order
+x=x-j\f %fifth order
